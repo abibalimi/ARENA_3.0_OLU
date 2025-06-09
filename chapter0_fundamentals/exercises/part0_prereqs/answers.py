07c744355bb29c16078f4db4207c144b8e348895
Olushola <abib.alimi@gmail.com>
2025-06-09 22:40:49 +0000

    part0_prereqs: setup and data loading

--- chapter0_fundamentals/exercises/part0_prereqs/answers.py
+++ chapter0_fundamentals/exercises/part0_prereqs/answers.py
@@ -1,7 +1,25 @@
+import math
 import os
 import sys
 from pathlib import Path
 
+import einops
+import numpy as np
+import torch as t
+from torch import Tensor
 
-if __name__  == "__main_":
-    pass
+# Make sure exercises are in the path
+chapter = "chapter0_fundamentals"
+section = "part0_prereqs"
+root_dir = next(p for p in Path.cwd().parents if (p / chapter).exists())
+exercises_dir = root_dir / chapter / "exercises"
+section_dir = exercises_dir / section
+if str(exercises_dir) not in sys.path:
+    sys.path.append(str(exercises_dir))
+
+import part0_prereqs.tests as tests
+from part0_prereqs.utils import display_array_as_img, display_soln_array_as_img
+
+MAIN = __name__ == "__main__"
+
+arr = np.load(section_dir / "numbers.npy")
