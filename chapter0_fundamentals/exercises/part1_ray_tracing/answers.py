f013343298773cbdb72a0e6a67ed24a086fe3fa5
Olushola <abib.alimi@gmail.com>
2025-06-18 22:42:39 +0000

    part1_ray_tracing: Ray-Object intersection - interactive update func.

--- chapter0_fundamentals/exercises/part1_ray_tracing/answers.py
+++ chapter0_fundamentals/exercises/part1_ray_tracing/answers.py
@@ -41,7 +41,7 @@ Debugging, with VSCode's built-in run & debug features"""
 # 1️⃣ Rays & Segments
 ## 1D Image Rendering
 
-## Exercise - implement make_rays_1d
+### Exercise - implement make_rays_1d
 
 ### Difficulty: 🔴🔴⚪⚪⚪
 ### Importance: 🔵🔵🔵⚪⚪
@@ -61,14 +61,37 @@ def make_rays_1d(num_pixels: int, y_limit: float) -> Tensor:
         [[0, 0, 0], [1, 0.75, 0]],
         [[0, 0, 0], [1, 1, 0]],
     ]
+    tip: the 'out' keyword argument: If provided, instead of allocating a new tensor and returning that, the output is written directly to the out tensor.
     """
     rays = t.zeros((num_pixels, 2, 3), dtype=t.float32)
-    print(f'before : {rays}')
     t.linspace(start=-y_limit, end=y_limit, steps=num_pixels, out=rays[:, 1, 1])
-    print(f'mid : {rays}')
     rays[:, 1, 0] = 1
-    print(f'after : {rays}')
     return rays
 
 rays1d = make_rays_1d(9, 10.0)
 fig = render_lines_with_plotly(rays1d)
+
+
+
+## Ray-Object Intersection
+
+fig: go.FigureWidget = setup_widget_fig_ray()
+display(fig)
+
+@interact(v=(0.0, 6.0, 0.01), seed=list(range(10)))
+def update(v=0.0, seed=0):
+    t.manual_seed(seed)
+    L_1, L_2 = t.rand(2, 2)
+    P = lambda v: L_1 + v * (L_2 - L_1)
+    x, y = zip(P(0), P(6))
+    with fig.batch_update():
+        fig.update_traces({"x": x, "y": y}, 0)
+        fig.update_traces({"x": [L_1[0], L_2[0]], "y": [L_1[1], L_2[1]]}, 1)
+        fig.update_traces({"x": [P(v)[0]], "y": [P(v)[1]]}, 2)
+        
+### Exercise - implement intersect_ray_1d
+### Difficulty: 🔴🔴🔴⚪⚪
+### Importance: 🔵🔵🔵🔵⚪
+
+### You should spend up to 20-25 minutes on this exercise.
+### It involves some of today's core concepts - tensor manipulation, linear operations, etc.
